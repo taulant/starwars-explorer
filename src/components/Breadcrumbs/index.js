88f27100d134c246b288c5f1d1640aef000c618e
Taulant Sulko <taulant.sulko@gmail.com>
2022-07-31 09:52:59 -0400

    Improving breadcrumb component logic to not include links if not needed.

--- src/components/Breadcrumbs/index.js
+++ src/components/Breadcrumbs/index.js
@@ -2,13 +2,21 @@ import { Link } from "react-router-dom";
 
 const Breadcrumbs = (props) => {
   const className = "Breadcrumbs";
-
+  const isHome = window.location.pathname === "/";
   return (
     <div className={className}>
       <h2>
-        <Link to="/">Home</Link>
-        {props.category ? `/ ${props.category}` : ""}
-        {props.name ? `/ ${props.name}` : ""}
+        {isHome ? "" : <Link to="/">Home</Link>}
+        {props.category && props.name ? (
+          <>
+            /<Link to={`/${props.category}`}>{props.category}</Link>
+          </>
+        ) : props.category ? (
+          `/${props.category}`
+        ) : (
+          ""
+        )}
+        {props.name ? `/${props.name}` : ""}
       </h2>
     </div>
   );
