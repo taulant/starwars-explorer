c58bb7d17658cd61f12e0010275a9859efc5bac4
Taulant Sulko <taulant.sulko@gmail.com>
2022-07-31 11:13:34 -0400

    Creating pagination component and implementing pagination logic on top of swapi.dev API pagination.

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,6 +1,18 @@
 const Pagination = (props) => {
   const className = "Pagination";
-  return <div>{className}</div>;
+  return (
+    <div>
+      <h3>
+        {props.responseData && (
+          <>
+            {props.currentPage} / {props.maxPages}
+          </>
+        )}
+      </h3>
+      <button onClick={() => props.prevPage()}>Previous Page</button>
+      <button onClick={() => props.nextPage()}>Next Page</button>
+    </div>
+  );
 };
 
 export default Pagination;
--- src/containers/Category/index.js
+++ src/containers/Category/index.js
@@ -1,14 +1,62 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 import Breadcrumbs from "../../components/Breadcrumbs";
 import Header from "../../components/Header";
+import Pagination from "../../components/Pagination";
 import Table from "../../components/Table";
 
 const CategoryContainer = (props) => {
   const className = "CategoryContainer";
+  const [responseData, setResponseData] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  // const { id } = useParams();
+  const { category } = props;
+  const results = responseData?.results;
+  const count = responseData?.count;
+  const resultsLength = results?.length;
+  const maxPages = Math.round(parseInt(count) / resultsLength);
 
+  const fetchData = useCallback(() => {
+    axios({
+      method: "GET",
+      url: `https://swapi.dev/api/${category}/?page=${currentPage}`,
+    })
+      .then((response) => {
+        setResponseData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [currentPage]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData, currentPage]);
+
+  const nextPage = () => {
+    if (currentPage < maxPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
   return (
     <>
       <Header />
       <Breadcrumbs category={props.category} />
+      {responseData ? JSON.stringify(responseData.results) : <p>Loading...</p>}
+      {responseData && (
+        <Pagination
+          responseData={responseData}
+          currentPage={currentPage}
+          maxPages={maxPages}
+          prevPage={prevPage}
+          nextPage={nextPage}
+        />
+      )}
       <Table category={props.category} />
     </>
   );
